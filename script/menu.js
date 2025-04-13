bbaeed74aa54f5b272aef72f17569d525842766c
Teddy Blomgren <Teddy_95@live.se>
2025-04-13 18:30:00 +0200

    first commit

---
+++ script/menu.js
@@ -0,0 +1,25 @@
+function buildMenu() {
+    const links = [
+      ["Hem", "index.html"],
+      ["Om oss", "aboutus.html"],
+      ["Kontakt", "contactus.html"]
+    ];
+  
+    const menu = document.getElementById("menu");
+  
+    if (menu) {
+      for (let i = 0; i < links.length; i++) {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        a.href = links[i][1];
+        a.textContent = links[i][0];
+        li.appendChild(a);
+        menu.appendChild(li);
+      }
+    } else {
+      console.error("Menyn hittades inte.");
+    }
+  }
+  
+  window.onload = buildMenu;
+  
